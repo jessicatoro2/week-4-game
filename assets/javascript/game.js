bac6be6a1552a2795485d55dc7c60ad21d3851be
Jessica Toro <jessica.toro@northwestern.edu>
2017-09-20 13:38:36 -0500

    Clean Code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@ var losses = 0;
 var RandomNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19 );
 
 // FUNCTIONS 
-// When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
+// When the game begins, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
 var startGame = function(){
   // the computer picks a random number between 19 and 120
   RandomNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19 );
@@ -19,39 +19,32 @@ var startGame = function(){
   $("#number-to-reach").text(RandomNumber);
 
   // CLICK FUNCTIONS
+
   $("#gemYellowpic").click(function(){
     userCounter += gemYellowCounter;
-    console.log("Yellow is worth", gemYellowCounter);
-    console.log("score is now: ", userCounter);
     $("#totalScore").text(userCounter);
     checkScore();
   });
 
   $("#gemGreenpic").click(function(){
     userCounter += gemGreenCounter;
-    console.log("GREEN is worth", gemGreenCounter);
-    console.log("score is now: ", userCounter);
     $("#totalScore").text(userCounter);
     checkScore();
   });
 
   $("#gemPinkpic").click(function(){
     userCounter += gemPinkCounter;
-    console.log("PINK is worth", gemPinkCounter);
-    console.log("score is now: ", userCounter);
     $("#totalScore").text(userCounter);
     checkScore();
   });
 
   $("#gemBluepic").click(function(){
     userCounter += gemBlueCounter;
-    console.log("BLUE is worth", gemBlueCounter);
-    console.log("score is now: ", userCounter);
     $("#totalScore").text(userCounter);
     checkScore();
   });
 
-  function checkScore(){
+  var checkScore = function(){
     if(userCounter === RandomNumber) {
       wins++;
       userCounter = 0;
@@ -65,7 +58,6 @@ var startGame = function(){
       losses++;
       userCounter = 0;
       $("#totalScore").text(userCounter);
-      // alert("Sorry no match, game over")
       console.log("losses", losses);
       $("#lossesTrack").text(losses);
       userCounter=0;
